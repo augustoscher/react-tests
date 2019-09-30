2597ff0d0b05d12bc50a2545b485129113ef0c40
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-09-29 21:50:41 -0300

    prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 import Header from './components/Header';
 
 function App() {
-  return (
-    <Header name='Test'></Header>
-  );
+  return <Header name="Test" />;
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Header.module.scss';
 
-const Header = ({ name }) => (
-  <div className={styles.header}>
-    {name}
-  </div>
-);
+const Header = ({ name }) => <div className={styles.header}>{name}</div>;
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 
 export default Header;
